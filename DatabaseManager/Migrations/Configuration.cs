ec3a37621f262769fa23469c5a893c9f76e6ad21
pawel.marcin.chojnacki@gmail.com <pawel.marcin.chojnacki@gmail.com>
2018-06-17 19:30:00 +0200

    Automatic migrations are now enabled.

--- DatabaseManager/Migrations/Configuration.cs
+++ DatabaseManager/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DatabaseManager.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(WeatherDataContext context)
