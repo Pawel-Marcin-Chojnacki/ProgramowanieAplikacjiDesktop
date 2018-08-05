b0a7d70ba9af93505de00198484ecdd1309d625b
pawel.marcin.chojnacki@gmail.com <pawel.marcin.chojnacki@gmail.com>
2018-08-05 19:44:28 +0200

    Changed way of creation request links.

--- OpenWeatherMap/Requests.cs
+++ OpenWeatherMap/Requests.cs
@@ -11,7 +11,7 @@ namespace OpenWeatherMap
     /// </summary>
     public class Requests
     {
-        private readonly string Forecast5DayAPI = "api.openweathermap.org/data/2.5/forecast";
+        private readonly string Forecast5DayAPI = "api.openweathermap.org/data/2.5/forecast?id=";
         private HttpClient client = new HttpClient();
         private string requestAddress;
 
@@ -19,27 +19,20 @@ namespace OpenWeatherMap
         /// Retrieves 5 days forecast for one city.
         /// </summary>
         /// <param name="cityId">The id number of the selected city.</param>
-        /// <returns>Forecast for 5 days.</returns>
-        public async Task<Forecast5> GetWeatherAsync(string cityName)
+        /// <returns>Forecast for 5 days for a given city.</returns>
+        public async Task<Forecast5> GetWeatherAsync(int cityId)
         {
-            requestAddress = Get5DaysWeatherRequest(cityName);
+            requestAddress = Get5DaysWeatherRequest(cityId);
             var result = await client.GetAsync(requestAddress);
             var resultContent = result.Content.ToString();
             var forecast = JsonConvert.DeserializeObject<Forecast5>(resultContent);
             return forecast;
         }
 
-        private string Get5DaysWeatherRequest(string cityName)
+        private string Get5DaysWeatherRequest(int cityId)
         {
-            int cityId = GetCityIdFromName(cityName);
-            string url = Forecast5DayAPI + "?" + cityId.ToString() + "&" + Credentials.ApiKey;
+            string url = Forecast5DayAPI + cityId.ToString() + "&APPID=" + Credentials.ApiKey;
             return url;
         }
-
-        private int GetCityIdFromName(string Name)
-        {
-            return 0;
-        }
-
     }
 }
