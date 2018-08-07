8d918263a32080cc0fa427b0e6ff9f85a30530c8
pawel.marcin.chojnacki@gmail.com <pawel.marcin.chojnacki@gmail.com>
2018-08-07 18:36:02 +0200

    Seed adds several cities for observation.

--- DatabaseManager/Migrations/Configuration.cs
+++ DatabaseManager/Migrations/Configuration.cs
@@ -11,7 +11,6 @@ namespace DatabaseManager.Migrations
     {
         public Configuration(DbModelBuilder modelBuilder) : base (modelBuilder)
         {
-            //AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(WeatherDataContext context)
@@ -27,18 +26,6 @@ namespace DatabaseManager.Migrations
                 new City { Name = "Berlin", Observed = true, ServiceId = 3081368 },
                 new City { Name = "Paryż", Observed = true, ServiceId = 3081368 }
                 );
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
